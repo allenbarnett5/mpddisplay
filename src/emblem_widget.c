78c30abe9eb75397e2ad561726397047fcea283f
Allen Barnett <allenbarnett5@gmail.com>
2022-01-02 15:29:50 -0500

    A somewhat nicer looking set of emblems using signed distance functions.

--- src/emblem_widget.c
+++ src/emblem_widget.c
@@ -15,6 +15,7 @@ struct EMBLEM_WIDGET_PRIVATE {
   GLuint texture;
   GLuint program;
   GLuint VAO;
+  GLuint emblem_u;
 }; 
 
 struct EMBLEM_WIDGET_HANDLE emblem_widget_init ( float x_mm, float y_mm,
@@ -33,19 +34,66 @@ struct EMBLEM_WIDGET_HANDLE emblem_widget_init ( float x_mm, float y_mm,
     "uniform mat4 mv_matrix;\n"
     "in  vec2 vertex;\n"
     "in  vec3 coloruv;\n"
+    "in  vec2 corneruv;\n"
     "out vec3 color;\n"
+    "out vec2 corner;\n"
     "void main(void) {\n"
     "  gl_Position = mv_matrix * vec4( vertex.x, vertex.y, 0., 1. );\n"
+    "  corner      = corneruv;\n"
     "  color       = coloruv;\n"
     "}";
 
+  // Draw the selected emblem.
   const GLchar* fragment_shader_source =
     "#version 300 es\n"
     "precision mediump float;\n"
+    "uniform int emblem;\n"
     "in vec3 color;\n"
+    "in vec2 corner;\n"
     "out vec4 fragColor;\n"
+    "void sdOctagon ( in vec2 p, in float r, out float d ) {\n"
+    "  const vec3 k = vec3( -0.9238795325, 0.3826834323, 0.4142135623 );\n"
+    "  p = abs(p);\n"
+    "  p -= 2.0*min(dot(vec2( k.x,k.y),p),0.0)*vec2( k.x,k.y);\n"
+    "  p -= 2.0*min(dot(vec2(-k.x,k.y),p),0.0)*vec2(-k.x,k.y);\n"
+    "  p -= vec2( clamp( p.x, -k.z*r, k.z*r), r );\n"
+    "  d = length(p) * sign(p.y);\n"
+    "}\n"
+    "void sdEquilateral ( in vec2 p, in float r, out float d ) {\n"
+    "  const float k = sqrt(3.);\n"
+    "  p.x = abs(p.x) - r;\n"
+    "  p.y = p.y + r/k;\n"
+    "  if ( p.x +  k * p.y > 0. ) p = vec2( p.x-k*p.y, -k*p.x-p.y ) / 2.;\n"
+    "  p.x -= clamp( p.x, -2.*r, 0. );\n"
+    "  d = -length(p) * sign( p.y );\n"
+    "}\n"
+    "void sdPause ( in vec2 p, in vec2 b, out float d ) {\n"
+    "  // Somewhat repetitive...\n"
+    "  vec2 ql = abs(p+vec2(0.5,0.)) - b;\n"
+    "  vec2 qr = abs(p-vec2(0.5,0.)) - b;\n"
+    "  float dl = length(max(ql,0.)) + min(max(ql.x,ql.y),0.);\n"
+    "  float dr = length(max(qr,0.)) + min(max(qr.x,qr.y),0.);\n"
+    "  d = min( dl, dr );\n"
+    "}\n"
     "void main(void) {\n"
-    "  fragColor.rgba = vec4( color, 1.f );\n"
+    "  float d;\n"
+    "  const float EPS = 2.5e-2;\n"
+    "  const vec2 b = vec2( .25, 0.9 );\n"
+    "  switch( emblem ) {\n"
+    "  case 0:\n"
+    "    sdOctagon( corner, 0.9, d );\n"
+    "    break;\n"
+    "  case 1:\n"
+    "    // Call with 90degree rotation\n"
+    "    sdEquilateral( vec2(-corner.y,corner.x), 0.9, d );\n"
+    "    break;\n"
+    "  case 2:\n"
+    "    sdPause( corner, b, d );\n"
+    "    break;\n"
+    "  }\n"
+    "  // Not the fanciest drawing one might imagine\n"
+    "  vec3 col  = abs(d) < EPS ? vec3(1.) : color;\n"
+    "  fragColor = vec4( col, d < EPS ? 1. : 0. );\n"
     "}";
 
   handle.d->program = OGLEScreateProgram( vertex_shader_source,
@@ -55,11 +103,9 @@ struct EMBLEM_WIDGET_HANDLE emblem_widget_init ( float x_mm, float y_mm,
 
   GLuint vertex_attr = glGetAttribLocation( handle.d->program, "vertex" );
   GLuint coloruv_attr = glGetAttribLocation( handle.d->program, "coloruv" );
+  GLuint corneruv_attr = glGetAttribLocation( handle.d->program, "corneruv" );
   GLuint mv_matrix_u = glGetUniformLocation( handle.d->program, "mv_matrix" );
-
-  printf( "vertex attr: %d\n", vertex_attr );
-  printf( "coloruv attr: %d\n", coloruv_attr );
-  printf( "matrix u: %d\n", mv_matrix_u );
+  handle.d->emblem_u = glGetUniformLocation( handle.d->program, "emblem" );
 
   // Include the translation (in mm) of the emblem in the modelview
   // operation. For this one, I'm putting the mm scale in the
@@ -70,202 +116,65 @@ struct EMBLEM_WIDGET_HANDLE emblem_widget_init ( float x_mm, float y_mm,
      0.f,                          0.f,                            1.f, 0.f,
      2.f*x_mm/screen_width_mm-1.f, 2.f*y_mm/screen_height_mm-1.f,  0.f, 1.f
   };
+  glUniform1i( handle.d->emblem_u, EMBLEM_WIDGET_EMBLEM_NOEMBLEM );
   glUniformMatrix4fv( mv_matrix_u, 1, GL_FALSE, mv_matrix );
 
   glGenVertexArrays( 1, &handle.d->VAO );
   glBindVertexArray( handle.d->VAO );
-  GLuint vertex_buffer, index_buffer, coloruv_buffer/*, corneruv_buffer*/;
+  GLuint vertex_buffer, index_buffer, coloruv_buffer, corneruv_buffer;
   glGenBuffers( 1, &vertex_buffer );
   glGenBuffers( 1, &index_buffer );
   glGenBuffers( 1, &coloruv_buffer );
-  printf( "emblem vertex buffer: %d\n", vertex_buffer );
-  printf( "emblem color buffer: %d\n", coloruv_buffer );
+  glGenBuffers( 1, &corneruv_buffer );
 
   // Draw in mm.
-  // Octagon corners.
-
-  const float OS = ( sqrtf(2.f) - 1.f ) / 2.f;
-
-  GLfloat square_vertexes[][2] =
+  GLfloat square_vertexes[4][2] =
     {
-     // Enter the octagon.
-     { 1.f, 0.5f+OS },
-     { 0.5f+OS, 1.f },
-     { 0.5f-OS, 1.f },
-     { 0.f, 0.5f+OS },
-     { 0.f, 0.5f-OS },
-     { 0.5f-OS, 0.f },
-     { 0.5f+OS, 0.f },
-     { 1.f, 0.5f-OS },
-     // Trianguly.
-     { 0.f, 0.f  },
-     { 1.f, 0.5f },
-     { 0.f, 1.f },
-     // Pause, please. Tricky.
-     { 0.f, 0.f }, // Left bulb.
-     { 0.375, 0.f },
-     { 0.375, 1.f },
-     { 0.f, 1.f },
-     { 0.625f, 0.f }, // Right bulb.
-     { 1.f, 0.f },
-     { 1.f, 1.f },
-     { 0.625f, 1.f },
+     { 0.f,      0.f },
+     { width_mm, 0.f },
+     { width_mm, height_mm },
+     { 0.f,      height_mm },
     };
 
-  const float T = 1.f; // thickness, mm
-
-  // Save some typing.
-  GLfloat  vbuf[16+6+16][2];
-  GLushort ibuf[18+8+18];
-  GLfloat (*vertexes)[2] = &vbuf[0];
-  GLushort* indexes = &ibuf[0];
-
-  // Octagon.
-  //  printf( "octagon\n" );
-  const float OR = 10.f / sqrtf(2.f + sqrt(2.f));
-  const float CRX = 5.f;
-  const float CRY = 5.f;
-  for ( int i = 0; i < 8; ++i ) {
-    const float theta = M_PI * (2*i+1) / 8.f;
-    const float cosx = cos(theta);
-    const float sinx = sin(theta);
-    //    printf( "%f: %f %f\n", theta, OR*cosx+CRX, OR*sinx+CRY );
-    vertexes[2*i][0] = OR*cosx+CRX;
-    vertexes[2*i][1] = OR*sinx+CRY;
-    vertexes[2*i+1][0] = (OR-T)*cosx+CRX;
-    vertexes[2*i+1][1] = (OR-T)*sinx+CRY;
-    indexes[2*i] = 2*i;
-    indexes[2*i+1] = 2*i+1;
-  }
-  indexes[2*8] = 0;
-  indexes[2*8+1] = 1;
-
-  // Triangle.
-  //  printf( "triangle\n" );
-  vertexes = &vbuf[16];
-  indexes  = &ibuf[18];
-  const float OT = 10.f / sqrtf(3.f);
-  const float CTX = 10.f / (2.f* sqrt(3.f));
-  const float CTY = 5.f;
-  for ( int i = 0; i < 3; ++i ) {
-    const float theta = M_PI * (2*i) / 3.f;
-    const float cosx = cos(theta);
-    const float sinx = sin(theta);
-    //    printf( "%f: %f %f\n", theta, OT*cosx+CTX, OT*sinx+CTY );
-    vertexes[2*i][0] = OT*cosx+CRX;
-    vertexes[2*i][1] = OT*sinx+CRY;
-    vertexes[2*i+1][0] = (OT-T)*cosx+CRX;
-    vertexes[2*i+1][1] = (OT-T)*sinx+CRY;
-    indexes[2*i] = 2*i + 16;
-    indexes[2*i+1] = 2*i+1 + 16;
-  }
-  indexes[2*3] = 0 + 16;
-  indexes[2*3+1] = 1 + 16;
-
-  // Pause (||).
-  //  printf( "pause\n" );
-  vertexes += 6;
-  indexes  += 8;
-  // Start with a template shape defined at the origin.
-  vertexes[0][0] = 0.f;
-  vertexes[0][1] = 0.f;
-  vertexes[1][0] = cos(M_PI/4.f) * T;
-  vertexes[1][1] = sin(M_PI/4.f) * T;
-  indexes[0] = 0 + 22;
-  indexes[1] = 1 + 22;
-  vertexes[2][0] = 3.f;
-  vertexes[2][1] = 0.f;
-  vertexes[3][0] = 3.f - cos(M_PI/4.f) * T;
-  vertexes[3][1] = sin(M_PI/4.f) * T;
-  indexes[2] = 2 + 22;
-  indexes[3] = 3 + 22;
-  vertexes[4][0] = 3.f;
-  vertexes[4][1] = 10.f;
-  vertexes[5][0] = 3.f - cos(M_PI/4.f) * T;
-  vertexes[5][1] = 10.f - sin(M_PI/4.f) * T;
-  indexes[4] = 4 + 22;
-  indexes[5] = 5 + 22;
-  vertexes[6][0] = 0.f;
-  vertexes[6][1] = 10.f;
-  vertexes[7][0] = cos(M_PI/4.f) * T;
-  vertexes[7][1] = 10.f - sin(M_PI/4.f) * T;
-  indexes[6] = 6 + 22;
-  indexes[7] = 7 + 22;
-  indexes[8] = 0 + 22;
-  indexes[9] = 1 + 22;
-
-  for ( int i = 0; i < 36; ++i ) {
-    if ( i < 30 ) {
-      printf( "%d %12f %12f %d\n", i, vbuf[i][0], vbuf[i][1], ibuf[i] );
-    }
-    else {
-      printf( "%d                          %d\n", i, ibuf[i] );
-    }
-  }
-  
   // Remember: if you pass 0 to glVertexAttribPointer, you must
   // have bound the buffer already.
   glBindBuffer( GL_ARRAY_BUFFER, vertex_buffer );
   glVertexAttribPointer( vertex_attr, 2, GL_FLOAT, GL_FALSE,
                          sizeof(GLfloat[2]), 0 );
-#if 0
   glBufferData( GL_ARRAY_BUFFER, sizeof(square_vertexes),
                 square_vertexes, GL_STATIC_DRAW );
-#else
-  glBufferData( GL_ARRAY_BUFFER, sizeof(vbuf), vbuf, GL_STATIC_DRAW );
-#endif
   glEnableVertexAttribArray( vertex_attr );
 
-  // VAO is implicit(?) and doesn't need to be enabled(?)
-  GLushort square_indexes[] = {
-                                0,1,2,3,4,5,6,7,
-                                8,9,10,
-                                11, 12, 13, 14, 65535, 15, 16, 17, 18
-  };
-  glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, index_buffer );
-#if 0
-  glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof(square_indexes),
-                square_indexes, GL_STATIC_DRAW );
-#else
-  glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof(ibuf), ibuf, GL_STATIC_DRAW );
-#endif
-#if 0
-  // Corner colors.
-  GLfloat square_coloruvs[][3] =
+  // Corner colors. (Like thermometer...)
+  GLfloat square_coloruvs[4][3] =
     {
-     // The Octagon.
-     { 1.f, 0.f, 1.f },
-     { 1.f, 0.f, 0.f },
-     { 1.f, 0.f, 0.f },
-     { 0.f, 0.f, 1.f },
-     { 1.f, 0.f, 0.f },
-     { 1.f, 0.f, 0.f },
-     { 1.f, 0.f, 0.f },
-     { 0.f, 1.f, 0.f },
-     // Trianguly.
-     { 1.f, 0.f, 1.f },
-     { 1.f, 0.f, 1.f },
-     { 1.f, 0.f, 1.f },
-     // Pause, please.
-     { 0.f, 1.f, 1.f },
-     { 0.f, 1.f, 1.f },
-     { 0.f, 1.f, 1.f },
-     { 0.f, 1.f, 1.f },
-     { 0.f, 1.f, 1.f },
-     { 0.f, 1.f, 1.f },
-     { 0.f, 1.f, 1.f },
-     { 0.f, 1.f, 1.f },
+     { 0.25f, 0.25f, 0.25f },
+     { 0.25f, 0.25f, 0.25f },
+     { 0.75f, 0.75f, 0.75f },
+     { 0.75f, 0.75f, 0.75f },
     };
-
   glBindBuffer( GL_ARRAY_BUFFER, coloruv_buffer );
   glVertexAttribPointer( coloruv_attr, 3, GL_FLOAT, GL_FALSE,
                          sizeof(GLfloat[3]), 0 );
   glBufferData( GL_ARRAY_BUFFER, sizeof(square_coloruvs),
                 square_coloruvs, GL_STATIC_DRAW );
   glEnableVertexAttribArray( coloruv_attr );
-#else
-  glVertexAttrib3f( coloruv_attr, 0.f, 1.f, 1.f );
-#endif
+
+  // Corner uv.
+  GLfloat square_corneruvs[4][2] =
+    {
+     { -1.f, -1.f },
+     {  1.f, -1.f },
+     {  1.f,  1.f },
+     { -1.f,  1.f },
+    };
+  glBindBuffer( GL_ARRAY_BUFFER, corneruv_buffer );
+  glVertexAttribPointer( corneruv_attr, 2, GL_FLOAT, GL_FALSE,
+                         sizeof(GLfloat[2]), 0 );
+  glBufferData( GL_ARRAY_BUFFER, sizeof(square_corneruvs),
+                square_corneruvs, GL_STATIC_DRAW );
+  glEnableVertexAttribArray( corneruv_attr );
+
   glBindVertexArray( 0 );
 
   return handle;
@@ -277,9 +186,10 @@ void emblem_widget_set_emblem ( struct EMBLEM_WIDGET_HANDLE handle,
   if ( handle.d == NULL )
     return;
 
-  printf( "Hello: %d\n", emblem );
-
   handle.d->emblem = emblem;
+
+  glUseProgram( handle.d->program );
+  glUniform1i( handle.d->emblem_u, emblem );
 }
 
 void emblem_widget_draw_emblem ( struct EMBLEM_WIDGET_HANDLE handle )
@@ -293,39 +203,10 @@ void emblem_widget_draw_emblem ( struct EMBLEM_WIDGET_HANDLE handle )
 
   glBindVertexArray( handle.d->VAO );
 
-  switch ( handle.d->emblem ) {
-  case EMBLEM_WIDGET_EMBLEM_STOPPED:
-#if 0
-    glDrawElements( GL_LINE_LOOP, 8, GL_UNSIGNED_SHORT, 0 );
-#else
-    glDrawElements( GL_TRIANGLE_STRIP, 18, GL_UNSIGNED_SHORT, 0 );
-#endif
-    break;
-  case EMBLEM_WIDGET_EMBLEM_PLAYING:
-#if 0
-    glDrawElements( GL_LINE_LOOP, 3, GL_UNSIGNED_SHORT,
-                    (void*)(8*sizeof(GLushort)) );
-#else
-    glDrawElements( GL_TRIANGLE_STRIP, 8, GL_UNSIGNED_SHORT, 
-                    (void*)(18*sizeof(GLushort)) );
-#endif
-    break;
-  case EMBLEM_WIDGET_EMBLEM_PAUSED:
-#if 0
-    glEnable(GL_PRIMITIVE_RESTART_FIXED_INDEX);
-    glDrawElements( GL_LINE_LOOP, 9, GL_UNSIGNED_SHORT,
-                    (void*)((8+3)*sizeof(GLushort)) );
-    glDisable(GL_PRIMITIVE_RESTART_FIXED_INDEX);
-#elif 1
-    glDrawElements( GL_TRIANGLE_STRIP, 10, GL_UNSIGNED_SHORT,
-                    (void*)((18+8)*sizeof(GLushort)) );
-#else
-    glDrawElements( GL_TRIANGLE_STRIP, 18, GL_UNSIGNED_SHORT, 0 );
-#endif
-    break;
-  case EMBLEM_WIDGET_EMBLEM_NOEMBLEM:
-    break;
-  }
+  glEnable( GL_BLEND );
+  glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+  glDrawArrays( GL_TRIANGLE_FAN, 0, 4 );
+  glDisable( GL_BLEND );
 }
 
 void emblem_widget_free_handle ( struct EMBLEM_WIDGET_HANDLE handle )
